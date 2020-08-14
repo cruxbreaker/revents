0d17f9b0b3076ca63f758eebdd874d3724f48a2e
cruxbreaker <geekankit318@gmail.com>
2020-08-14 06:47:15 +0530

    Added props and state to EventDashboard

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 import 'semantic-ui-css/semantic.min.css';
-import { Container } from "semantic-ui-react";
+import { Container } from 'semantic-ui-react';
 
-import EventsDashboard from "./views/EventsDashboard";
-import Navbar from "./components/Navbar";
+import EventDashboard from './views/EventDashboard';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
       <Navbar />
-      <Container className="main">
-        <EventsDashboard />
+      <Container className='main'>
+        <EventDashboard />
       </Container>
     </>
   );
--- src/components/EventList/EventAttendee.js
+++ src/components/EventList/EventAttendee.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { List, Image } from 'semantic-ui-react';
 
-function EventAttendee() {
-    return (
-        <List.Item>
-            <Image as='a' size='mini' circular src='https://randomuser.me/api/portraits/women/42.jpg' />
-        </List.Item>
-    )
+function EventAttendee({ attendee }) {
+  return (
+    <List.Item>
+      <Image as='a' size='mini' circular src={attendee.photoURL} />
+    </List.Item>
+  );
 }
 
-export default EventAttendee
+export default EventAttendee;
